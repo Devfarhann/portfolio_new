7575efafe4a6ffabb66da48e72272b00356faa66
Devfarhann <134830918+Devfarhann@users.noreply.github.com>
2023-09-21 13:29:08 +0530

    Add files via upload

---
+++ precache-manifest.0b97be94b6e6f97cc7cfba73fd699721.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9749af683aec1f5074",
+    "url": "/static/css/main.570c8a8f.chunk.css"
+  },
+  {
+    "revision": "6d9749af683aec1f5074",
+    "url": "/static/js/main.6d9749af.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "88e7cce250b8ba822963",
+    "url": "/static/js/2.88e7cce2.chunk.js"
+  },
+  {
+    "revision": "b3d155d33fc8f3126447",
+    "url": "/static/js/runtime~main.b3d155d3.js"
+  },
+  {
+    "revision": "c750bdf6f4e32684e803579da63d04a5",
+    "url": "/static/media/Unicorn_logo.c750bdf6.jpg"
+  },
+  {
+    "revision": "96e469906cd2d579a01e7c92ab3b09bc",
+    "url": "/static/media/GOJO.96e46990.png"
+  },
+  {
+    "revision": "4b7bc0b1accc2d2a1445be273d121770",
+    "url": "/static/media/gojo left.4b7bc0b1.jpg"
+  },
+  {
+    "revision": "1fcdfa46f0d86204197199f44c2f2a04",
+    "url": "/static/media/twitter logo.1fcdfa46.png"
+  },
+  {
+    "revision": "c3fcc390427d68b89647e834adf7fc87",
+    "url": "/static/media/linkedin logo.c3fcc390.png"
+  },
+  {
+    "revision": "ac77eaa46cd68913c085d8321f1d09b8",
+    "url": "/static/media/insta.ac77eaa4.jpeg"
+  },
+  {
+    "revision": "2403f6dad85d8f6e9d63b48e4abf7042",
+    "url": "/static/media/Background image.2403f6da.webp"
+  },
+  {
+    "revision": "f7021c5b0fef8545853b80b2bf294969",
+    "url": "/static/media/tree.f7021c5b.jpeg"
+  },
+  {
+    "revision": "bf2b4bf052eb27a22ebf0e7eaa8666f7",
+    "url": "/static/media/pexels-fauxels-3183197.bf2b4bf0.jpg"
+  },
+  {
+    "revision": "094c34b7255d14f1c65bf7afaecdbb1e",
+    "url": "/index.html"
+  }
+];
